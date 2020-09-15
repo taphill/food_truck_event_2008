3c364abb75be3c9ffccd3a6fa047fdf5eea0aead
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 14:16:46 -0500

    Add basic tests for Item class

---
+++ test/item_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  def test_it_exists
+    assert_instance_of Item, @item1
+  end
+
+  def test_it_has_attributes
+    assert_equal "Apple Pie (Slice)", @item2.name
+    assert_equal 2.50, @item2.price  
+  end
+end

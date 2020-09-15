9e8df51f8c82ff699c390ac33e7e4a29aba89cd3
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 15:37:03 -0500

    Add test for total_inventory method

--- test/event_test.rb
+++ test/event_test.rb
@@ -79,4 +79,28 @@ class EventTest < Minitest::Test
     assert_equal expected, @event.food_trucks_that_sell(@item1)
     assert_equal [@food_truck2], @event.food_trucks_that_sell(@item4)
   end
+
+  def test_it_can_find_total_inventory
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expected = {
+      @item1 => { quantity: 100, food_trucks: [@food_truck1, @food_truck3] },
+      @item2 => { quantity: 7, food_trucks: [@food_truck1] },
+      @item4 => { quantity: 50, food_trucks: [@food_truck2] },
+      @item3 => { quantity: 35, food_trucks: [@food_truck2, @food_truck3] }
+    }
+
+    assert_equal expected, @event.total_inventory
+  end
 end

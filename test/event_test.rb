815aac4e4e24b7af8bfff21ad901ec8df64f33c7
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 14:58:27 -0500

    Add test for food_trucks_that_sell method

--- test/event_test.rb
+++ test/event_test.rb
@@ -60,4 +60,23 @@ class EventTest < Minitest::Test
 
     assert_equal expected, @event.food_truck_names
   end
+
+  def test_it_can_return_food_truck_names
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+
+    @food_truck3.stock(@item1, 65)
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expected = [@food_truck1, @food_truck3]
+
+    assert_equal expected, @event.food_trucks_that_sell(@item1)
+    assert_equal [@food_truck2], @event.food_trucks_that_sell(@item4)
+  end
 end

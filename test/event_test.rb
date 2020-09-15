1dc5d49a8a506d2ccd5cb5d60c799fa545e1658d
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 15:49:50 -0500

    Add test for sorted_item_list

--- test/event_test.rb
+++ test/event_test.rb
@@ -120,4 +120,23 @@ class EventTest < Minitest::Test
 
     assert_equal [@item1], @event.overstocked_items
   end
+
+  def test_it_can_return_sorted_item_list
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expected = ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
+
+    assert_equal expected, @event.sorted_item_list
+  end
 end

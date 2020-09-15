c6b006a3a1830a6b8727993eaffc7c3e35ec932e
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 15:37:18 -0500

    Add total_inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,30 @@ class Event
       truck
     end.compact
   end
+
+  def total_inventory
+    inventory = initialize_inventory_hash.each do |item, data|
+      food_trucks.each do |truck|
+        next unless truck.inventory.keys.include?(item)
+    
+        data[:quantity] += truck.inventory[item]
+        data[:food_trucks] << truck
+      end
+    end
+
+    inventory
+  end
+
+  private
+
+  def initialize_inventory_hash
+    inventory_hash = {}
+    food_trucks.each do |truck|
+      truck.inventory.each do |item, amount|
+        inventory_hash[item] = { quantity: 0, food_trucks: [] }
+      end
+    end
+
+    inventory_hash
+  end
 end
